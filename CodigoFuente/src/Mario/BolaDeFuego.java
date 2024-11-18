0c5ed6a7791742ea66d4b658e5ac125d283824b6
Esteban Lartigau <tebitalartigau@gmail.com>
2024-11-18 01:50:15 -0300

    Agrego rebote a bola de fuego

--- CodigoFuente/src/Mario/BolaDeFuego.java
+++ CodigoFuente/src/Mario/BolaDeFuego.java
@@ -13,33 +13,42 @@ public class BolaDeFuego extends EntidadDinamica implements VisitorBolaDeFuego{
 
 	protected Juego juego;
 	protected int gravedad;
+	protected int maximoY;
 	
 	public BolaDeFuego(Sprite sprite, int x, int y, int ancho, int alto, Juego juego) {
 		super(sprite, x, y, ancho, alto);
 		this.velocidad=6;
 		this.juego = juego;
 		this.gravedad=1;
+		this.maximoY = y;
 		
 	}
 	 
 	public void actualizarPosicion() {
 		posicion.setX(posicion.getX()+velocidad);
 		posicion.setY(posicion.getY()+gravedad);
-		this.notificarObserver();		
+		chequearMaximoY();
+		this.notificarObserver();
 	}
 
 	public void setOrientacion(int orientacion) {
 		velocidad = velocidad * orientacion;
 	}
+	
+	private void chequearMaximoY() {
+		if(this.getY() <= maximoY) {
+			gravedad = 1;
+		}
+	}
 
 	@Override
 	public void visit(Plataforma plataforma,int lado) {
+		
 		if(lado==DetectorColisiones.ABAJO_4) {
 			gravedad=gravedad*-1;
-		}
-		else {
-		this.destruir();
-		juego.getNivelActual().eliminarBolaDeFuego(this);
+		}else {
+			this.destruir();
+			juego.getNivelActual().eliminarBolaDeFuego(this);
 		}
 		
 		
